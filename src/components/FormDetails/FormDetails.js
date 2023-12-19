5e9608903145e11f7400008e2e85d10298c2a099
Povindu Samarasekara <povinduchanmith@gmail.com>
2023-12-19 15:55:16 +0530

    chanegs form detaila adding steps and changing phone number, style changes

--- src/components/FormDetails/FormDetails.js
+++ src/components/FormDetails/FormDetails.js
@@ -8,9 +8,20 @@ export default function FormDetails() {
       <div className='FDinner'>
 
         <div className="payment">
-          <h1 className='ticketPrice'> Ticket Price:  LKR:2000</h1>
+        {/* &nbsp; */}
+          <h1 className='ticketPrice'> Ticket Price <br/>  LKR 2000 </h1>
             <br/>
+
+            <h1 className='paymentMethod'>Registration Steps</h1>
+
+            1. Make the payment to the following account.<br/>
+            2. Fill the registration form and upload the payment slip.<br/>
+            3. Submit the the form<br/>
             <br/>
+            <p className='detailP'>After verifying the payment, you will recieve a confirmation email within few hours.<br/></p>
+            <br/>
+
+
             <h1 className='paymentMethod'>Payment Methods</h1>
             
             
@@ -19,15 +30,14 @@ export default function FormDetails() {
             • Holder's Name: N.A Abeygunawardena
             <br/>
             • Branch: Sampath Bank Matara Super Branch
-            <br/>
-            <br/>
+
         </div>
 
         <div className="contactInfo">
           For further clarifications please contact :
           <br/> Project Chairman
           <br/> Leo Ama Samarawikrama
-          <br/> 071889940
+          <br/> 0718899400
         </div>
       </div>
     </div>
